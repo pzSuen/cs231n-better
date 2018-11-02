6d749e417fcac19e6a8f46bfcaec1d5ff17db48b
PaulSuen <pz.suen@gmail.com>
2018-11-02 22:29:37 +0800

    finish softmax-navie

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -32,25 +32,51 @@ def softmax_loss_naive(W, X, y, reg):
     ##########################################################################
     num_example = X.shape[0]
     num_class = W.shape[1]
-    y = y.reshape(num_example, 1)
+    # y = y.reshape(num_example, 1)
 #   S = np.array(num_X).reshape(num_X,1)
-    for i in range(num_example):   # 遍历所有样本
-        S_sum = 0.0  # 存储各个类别的总和
-        si = np.dot(X[i, :], W)
-        e_si = np.exp(si)
-        for j in range(num_class):  # 遍历各个类别
-            S_sum += e_si
-        L_i = -np.log(e_si[y[i, 1]] / S_sum)    # 损失分值
-        loss+=L_i
-        # 计算dW
-        # for j in range(num_class):
-        #   if j!=y[i,1]:
-        #     dW[,j]=
+    for i in range(num_example):              # 遍历所有样本
+        # score = np.dot(X[i], W)
+        # score -= max(score)  # 为了数值稳定性
+        # score = np.exp(score)  # 取指数
+        # softmax_sum = np.sum(score)  # 得到分母
+        # score /= softmax_sum  # 除以分母得到softmax
+        # # 计算梯度
+        # for j in range(W.shape[1]):
+        #     if j != y[i]:
+        #         dW[:, j] += score[j] * X[i]
+        #     else:
+        #         dW[:, j] -= (1 - score[j]) * X[i]
 
+        # loss -= np.log(score[y[i]])  # 得到交叉熵
+        # print(loss)
+
+
+        scores_sum = 0.0                      # 存储各个类别的总和
+        scores_i = np.dot(X[i, :], W)
+        scores_i -= max(scores_i)                 # 为了数值的稳定性
+        scores_i = np.exp(scores_i)
+        scores_sum = np.sum(scores_i)             # 并计算总的score
+        scores_i /= scores_sum          # 真实类别所占的比例
+        loss_i = -np.log(scores_i[y[i]])        # 第i个样本的损失
+        loss += loss_i
+        # 计算dW
+        for j in range(num_class):
+            if j != y[i]:
+                dW[:, j] += scores_i[j] * X[i, :]
+            else:
+                dW[:, j] += (scores_i[j] - 1) * X[i, :]
+        # print(str(i)+":"+str(loss))
 
     ##########################################################################
     #                          END OF YOUR CODE                                 #
     ##########################################################################
+    loss /= num_example
+    dW /= num_example
+
+    loss += reg * np.sum(W*W)
+    dW += 2 * reg * W
+    # print(loss.shape)
+    # print(dW.shape)
 
     return loss, dW
 
